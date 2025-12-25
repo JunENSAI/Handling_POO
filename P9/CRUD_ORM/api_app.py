3354edd40c34d29a2e959d857c94d087d9e7db3c
Junior <josephson-junior.razafindratsiza@eleve.ensai.fr>
2025-12-25 22:52:36 +0100

    add final correction

---
+++ P9/CRUD_ORM/api_app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models, schemas, crud
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users", response_model=schemas.UserPublic)
+async def create_user_endpoint(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
